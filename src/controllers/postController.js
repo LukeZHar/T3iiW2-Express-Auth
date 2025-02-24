1369000e44b4fc2f4b8881ed120fe6858f2ad6ef
LukeZHar <15649@coderacademy.edu.au>
2025-02-24 15:06:17 +1000

    Feat: Post controller functions declared

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,1 +1,47 @@
-const User = require("../models/UserModel");
+const { response } = require("express");
+const Post = require("../models/PostModel");
+
+// CRUD operations to be defined here
+
+// Read - Get all posts
+async function getAllPosts(req, res) {
+    try {
+        const posts = await Post.find();
+        res
+        .status(200)
+        .json(posts);
+    } catch (error) {
+        res
+        .status(500)
+        .json({ 
+            error: error.message 
+        });
+    }
+};
+
+// Create - Create a new post
+async function createPost(req, res) {
+    try {
+        const { title, content } = req.body;
+        const post = await Post.create({
+            title,
+            content,
+            user: req.authUserData.userId
+        });
+
+        res
+        .status(201)
+        .json(post);
+    } catch (error) {
+        res
+        .status(500)
+        .json({ 
+            error: error.message 
+        });
+    }
+};
+
+module.exports = {
+    getAllPosts,
+    createPost
+}
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,14 +1,11 @@
 const express = require("express");
+const { getAllPosts, createPost } = require("../controllers/postController");
 const router = express.Router();
 
 // GET localhost:5000/api/posts/
-router.get("/", () => {
-    console.log("get all posts");
-});
+router.get("/", getAllPosts);
 
 // POST localhost:5000/api/posts/
-router.post("/", () => {
-    console.log("create post");
-});
+router.post("/", createPost);
 
 module.exports = router;
